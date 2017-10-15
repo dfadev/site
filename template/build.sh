aa46cb44cdb311218d32524f61cbda66eb820c28
Russ Panula <dfadev@dfa.net>
2017-10-15 13:31:07 -0500

    install npm modules if node_modules doesn't exist fix build order

--- template/build.sh
+++ template/build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-CLIENT="haxe hxml/client.hxml && site pack"
 APPSRV="haxe hxml/appserver.hxml"
 WEBSRV="haxe hxml/webserver.hxml"
 RENDERVIEW="haxe hxml/renderview.hxml"
 
-site hxml
-concurrently "$CLIENT" "$APPSRV" "$WEBSRV" "$RENDERVIEW"
-site html
+if [ ! -d node_modules ]; then
+	site npm
+fi
+
+site hxml && haxe hxml/client.hxml && site pack && concurrently "$APPSRV" "$WEBSRV" "$RENDERVIEW" && site html
